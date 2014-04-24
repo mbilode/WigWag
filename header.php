8f522d7343b349abe65e56f23b5681831ae44d14
mbilode <mlbilode@uvm.edu>
2014-04-24 11:40:08 -0400

    changes made tester

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+include 'top.php';
+?>
+
+<h1>This is WigWag</h1> <!--logo would go here-->
+
+    <div id='contactInfo'>
+        
+        <p> Email: mlbilode@uvm.edu</p>
+        
+        
+        
+        
+        <p>Phone: (802) 860-6630</p>
+        
+    </div>
---
+++ top.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <title>WigWag</title>  <!--displays what goes on the browser tab-->
+        <meta charset="utf-8">
+        <meta name="author" content="Monica Bilodeau">
+        <meta name="description" content="WigWag is a web developing group of college students using prior experience and looking for more. ">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet"
+              href="stylesheet.css"
+    type="text/css"
+media="screen">
+        
+    </head>
